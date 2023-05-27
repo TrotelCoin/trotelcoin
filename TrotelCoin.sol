f22b76190862b723644e7efe02e4157bdc64e890
atrtdeee <trotelalexandre@proton.me>
2023-05-27 12:01:13 +0200

    syntax

--- TrotelCoin.sol
+++ TrotelCoin.sol
@@ -8,16 +8,16 @@ contract TrotelCoin {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Burn(address indexed from, uint256 value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Burn(address indexed _from, uint256 _value);
 
     constructor(uint256 _totalSupply) {
         totalSupply = _totalSupply;
         balanceOf[msg.sender] = _totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         _transfer(msg.sender, _to, _value);
         return true;
