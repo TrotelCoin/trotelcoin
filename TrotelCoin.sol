5b0cd49521dfcfd0ba132b300e8982cad57b2dc3
atrtdeee <trotelalexandre@proton.me>
2023-05-27 12:22:28 +0200

    adding interface

--- TrotelCoin.sol
+++ TrotelCoin.sol
@@ -30,9 +30,17 @@ contract TrotelCoin {
         return totalSupply;
     }
 
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return ;
+    }
+
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         return true;
     }
 
