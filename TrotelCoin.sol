2f1d3b7a31e22868f6934be458a770f2a91bfba9
atrtdeee <trotelalexandre@proton.me>
2023-05-27 12:37:15 +0200

    fix

--- TrotelCoin.sol
+++ TrotelCoin.sol
@@ -35,7 +35,7 @@ contract TrotelCoin {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance")
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
