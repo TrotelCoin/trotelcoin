5c035fe7a5f5a911912e5028b492ff965225eec3
atrtdeee <trotelalexandre@proton.me>
2023-05-27 13:15:35 +0200

    fix

--- TrotelCoin.sol
+++ TrotelCoin.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "remix_tests.sol"; // Importez le fichier de bibliothèque pour les tests de Remix
-
 contract TrotelCoin {
     string public name = "TrotelCoin";
     string public symbol = "TROTEL";
