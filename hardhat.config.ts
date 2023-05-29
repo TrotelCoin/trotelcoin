92fa221cd770d72e4704923e352da12c273314b5
atrtdeee <trotelalexandre@proton.me>
2023-05-29 15:54:11 +0200

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,8 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   networks: {
     polygon: {
-      url: "https://polygon-rpc-url", // Remplacez par l'URL RPC fournie par Polygon
-      accounts: [privateKey] // Remplacez par votre clé privée du compte qui effectuera le déploiement
+      url: "https://polygon-rpc-url",
+      accounts: 123
     }
   },
   solidity: "0.8.0",
