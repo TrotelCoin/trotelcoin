f6a742c3211e3235a45397e8daa01ae80267930f
atrtdeee <trotelalexandre@proton.me>
2023-06-01 00:18:44 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {ethers} from 'ethers';
-// import TrotelCoin from './artifacts/contracts/TrotelCoin.sol/TrotelCoin.json';
 import './App.css';
 import { Button } from '@mui/material';
 
-const tokenAddress = "0xf04ab1a43cBA1474160B7B8409387853D7Be02d5";
-
 function App() {
-  const [balance, setBalance] = useState();
-
-  useEffect(() => {
-    getBalance();
-  }, [])
-
-  async function getBalance() {
-    if(typeof window.ethereum !== 'undefined') {
-      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
-      const provider = new ethers.providers.Web3Provider();
-      // const contract = new ethers.Contract(tokenAddress, TrotelCoin.abi, provider);
-      const balance = await contract.balanceOf(accounts[0]);
-      setBalance(balance.toString() / (10 ** contract.decimals));
-    }
-  }
-
   return (
     <div className="App">
       <p>Vous avez {balance} TROTEL.</p>
