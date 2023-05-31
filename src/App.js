ca83d9c782cb0180107e3d4c5bc030a936fd47ca
atrtdeee <trotelalexandre@proton.me>
2023-06-01 00:23:50 +0200

    adding app.js code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {ethers} from 'ethers';
-import './App.css';
+import MyGroup from '../components/mygroup.js';
+import walletConnectFunction from '../components/trotelcoin/walletConnect.js';
+import './styles/App.css';
 import { Button } from '@mui/material';
 
 function App() {
