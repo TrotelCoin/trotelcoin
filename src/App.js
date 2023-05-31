02df3e85ee42dae45c08b91eda7b8e18168688ed
atrtdeee <trotelalexandre@proton.me>
2023-05-31 17:45:08 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {ethers} from 'ethers';
 import TrotelCoin from './artifacts/contracts/TrotelCoin.sol/TrotelCoin.json';
 import './App.css';
 
-const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const tokenAddress = "0xf04ab1a43cBA1474160B7B8409387853D7Be02d5";
 
 function App() {
   const [balance, setBalance] = useState();
