d92a93916e49c7d1507218b55bcee4eb145ce691
atrtdeee <trotelalexandre@proton.me>
2023-06-01 00:25:35 +0200

    adding consts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import './styles/App.css';
 import { Button } from '@mui/material';
 
 function App() {
+  const [walletData, setWalletData] = useState();
+  const [account, setAccount] = useState();
+  const [network, setNetwork] = useState();
+  const [contractAddress, setContractAddress] = useState();
+
   return (
     <div className="App">
       <p>Vous avez {balance} TROTEL.</p>
