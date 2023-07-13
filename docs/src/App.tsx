156b0be5cdcaef85219327ce44ebbc23c1f2f8dc
atrtdeee <trotelalexandre@proton.me>
2023-07-12 23:06:19 -0500

    vite setup

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import community from './components/community/community';
-import footer from './components/footer/footer';
-import header from './components/header/header';
-import hero from './components/hero/hero';
-import learncard from './components/learncard/learncard';
-import token from './components/token/token';
+import { header, footer, header, hero, learncard, token } from "./components";
 
 export const TrotelCoin = () => {
   return (
---
+++ docs/src/components/index.ts
@@ -0,0 +1,8 @@
+import community from './components/community/community';
+import footer from './components/footer/footer';
+import header from './components/header/header';
+import hero from './components/hero/hero';
+import learncard from './components/learncard/learncard';
+import token from './components/token/token';
+
+export { community, footer, header, hero, learncard, token };
