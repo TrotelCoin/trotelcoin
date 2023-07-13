87c81b1bd02aa12147f57f338386108a2bc4d691
atrtdeee <trotelalexandre@proton.me>
2023-07-12 23:01:14 -0500

    vite setup

---
+++ docs/src/components/learncard/learncard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./learncard.css";
+
+const learncard = () => {
+  return (
+    <div>learncard</div>
+  )
+}
+
+export default learncard;
---
+++ docs/src/components/token/token.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./token.css";
+
+const token = () => {
+  return (
+    <div>token</div>
+  )
+}
+
+export default token;
