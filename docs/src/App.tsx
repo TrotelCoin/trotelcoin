f01afc159c0b6af4d001a4754cb3ed3313030a46
atrtdeee <trotelalexandre@proton.me>
2023-07-12 22:58:53 -0500

    vite setup

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export const TrotelCoin = () => {
   return (
@@ -6,4 +6,4 @@ export const TrotelCoin = () => {
   )
 }
 
-export default TrotelCoin
+export default TrotelCoin;
---
+++ docs/src/components/community/community.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./community.css";
+
+const community = () => {
+  return (
+    <div>community</div>
+  )
+}
+
+export default community;
