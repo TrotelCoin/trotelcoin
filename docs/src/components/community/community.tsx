0ce0d2f3d306ca396abc01a9d98a67919d44b0ec
atrtdeee <trotelalexandre@proton.me>
2023-07-13 05:44:05 -0500

    importing React

--- docs/src/components/community/community.tsx
+++ docs/src/components/community/community.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import CountUp from 'react-countup';
 import { useInView } from 'react-intersection-observer';
 import './Community.css';
