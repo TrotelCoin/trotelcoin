835b22c91b31cc3b23166ceb3146aadb19325c3a
atrtdeee <trotelalexandre@proton.me>
2023-07-13 06:43:53 -0500

    index.ts to index.tsx

---
+++ docs/src/components/index.tsx
@@ -0,0 +1,15 @@
+import Community from './Community/Community';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Hero from './Hero/Hero';
+import Learncard from './Learncard/Learncard';
+import Token from './Token/Token';
+
+export {
+  Community,
+  Footer,
+  Header,
+  Hero,
+  Learncard,
+  Token
+};
