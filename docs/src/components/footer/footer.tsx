2b28f0b2a26a96726e7a24b31283577110acc133
atrtdeee <trotelalexandre@proton.me>
2023-07-13 22:04:15 -0500

    finishing homepage without responsive

--- docs/src/components/footer/footer.tsx
+++ docs/src/components/footer/footer.tsx
@@ -3,29 +3,29 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <div className="trotelcoin-minimalist-footer"></div>
-      <div className="line-footer"></div>
-      <div className="trotelcoin">
-        <h3 className="footer-title">TrotelCoin</h3>
-        <a href="#"><p>Home</p></a>
-        <a href="../../about"><p>About</p></a>
-        <a href="https://app.trotelcoin.com"><p>The App</p></a>
+      <img className="trotelcoin-logo" src="../../assets/trotelcoin-minimalist.jpg" alt="TrotelCoin logo"></img>
+      <div className="footer-all-sections">
+        <div className="footer-section">
+          <h3 className="footer-title">TrotelCoin</h3>
+          <a href="#"><p className="footer-label">Home</p></a>
+          <a href="../../about"><p className="footer-label">About</p></a>
+          <a href="https://app.trotelcoin.com"><p className="footer-label">The App</p></a>
+        </div>
+        <div className="footer-section">
+          <h3 className="footer-title">Token</h3>
+          <a target="_blank" href="https://flooz.xyz/trade/how-to-buy/0xf04ab1a43cBA1474160B7B8409387853D7Be02d5?network=bsc"><p className="footer-label">Buy $TROTEL</p></a>
+          <a href="https://staking.trotelcoin.com"><p className="footer-label">Staking</p></a>
+          <a target="_blank" href="https://bscscan.com/token/0xf04ab1a43cBA1474160B7B8409387853D7Be02d5"><p className="footer-label">BNB Chain Explorer</p></a>
+          <a target="_blank" href="https://coinmarketcap.com/currencies/trotelcoin-v2/"><p className="footer-label">CoinMarketCap</p></a>
+        </div>
+        <div className="footer-section">
+          <h3 className="footer-title">Socials</h3>
+          <a target="_blank" href="https://twitter.com/trotelcoin"><p className="footer-label">Twitter</p></a>
+          <a target="_blank" href="https://discord.gg/zNCyUaVpmV"><p className="footer-label">Discord</p></a>
+          <a target="_blank" href="https://medium.com/@trotelcoin"><p className="footer-label">Medium</p></a>
+          <a target="_blank" href="https://instagram.com/trotelcoin"><p className="footer-label">Instagram</p></a>
+        </div>
       </div>
-      <div className="token-footer">
-        <h3 className="footer-title">Token</h3>
-        <a target="_blank" href="https://flooz.xyz/trade/how-to-buy/0xf04ab1a43cBA1474160B7B8409387853D7Be02d5?network=bsc"><p>Buy $TROTEL</p></a>
-        <a href="https://staking.trotelcoin.com"><p>Staking</p></a>
-        <a target="_blank" href="https://bscscan.com/token/0xf04ab1a43cBA1474160B7B8409387853D7Be02d5"><p>BNB Chain Explorer</p></a>
-        <a target="_blank" href="https://coinmarketcap.com/currencies/trotelcoin-v2/"><p>CoinMarketCap</p></a>
-      </div>
-      <div className="socials">
-        <h3 className="footer-title">Socials</h3>
-        <a target="_blank" href="https://twitter.com/trotelcoin"><p>Twitter</p></a>
-        <a target="_blank" href="https://discord.gg/zNCyUaVpmV"><p>Discord</p></a>
-        <a target="_blank" href="https://medium.com/@trotelcoin"><p>Medium</p></a>
-        <a target="_blank" href="https://instagram.com/trotelcoin"><p>Instagram</p></a>
-      </div>
-      <div className="rectangle-footer"></div>
     </footer>
   )
 }
--- docs/src/components/learncard/learncard.tsx
+++ docs/src/components/learncard/learncard.tsx
@@ -3,12 +3,11 @@ import "./Learncard.css";
 const Learncard = () => {
   return (
     <section className="learncard">
-      <div className="learn-card-text">
+      <div className="learn-card">
         <p className="ready-text">ARE YOU READY?</p>
         <h1 className="learn-crypto-now">Learn Crypto Now.</h1>
         <a href="https://app.trotelcoin.com"><button id="launch-app-card">Launch The App</button></a>
       </div>
-      <div id='rectangle-learn-card'></div>
     </section>
   )
 }
